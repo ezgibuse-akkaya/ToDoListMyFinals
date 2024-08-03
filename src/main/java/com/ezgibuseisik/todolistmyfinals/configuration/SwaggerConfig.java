f38a9c2bb0da37b1ffc7465930056549f9af237a
Ezgi Buse Işık <ezgibuse159@gmail.com>
2024-08-04 00:33:47 +0300

    swagger problem

--- src/main/java/com/ezgibuseisik/todolistmyfinals/configuration/SwaggerConfig.java
+++ src/main/java/com/ezgibuseisik/todolistmyfinals/configuration/SwaggerConfig.java
@@ -1,35 +1,31 @@
-//package com.ezgibuseisik.todolistmyfinals.configuration;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import springfox.documentation.builders.ApiInfoBuilder;
-//import springfox.documentation.builders.PathSelectors;
-//import springfox.documentation.builders.RequestHandlerSelectors;
-//import springfox.documentation.service.ApiInfo;
-//import springfox.documentation.spi.DocumentationType;
-//import springfox.documentation.spring.web.plugins.Docket;
-//import springfox.documentation.swagger2.annotations.EnableSwagger2;
-//
-//
-//@Configuration
-//@EnableSwagger2
-//public class SwaggerConfig {
-//
-//    @Bean
-//    public Docket api() {
-//        return new Docket(DocumentationType.SWAGGER_2)
-//                .select()
-//                .apis(RequestHandlerSelectors.basePackage("com.ezgibuseisik.todolistmyfinals.controller"))
-//                .paths(PathSelectors.any())
-//                .build()
-//                .apiInfo(apiInfo());
-//    }
-//
-//    private ApiInfo apiInfo() {
-//        return new ApiInfoBuilder()
-//                .title("Analyst Software Developer Ezgi Buse Işık")
-//                .description("Spring Boot & React Js")
-//                .version("1.0")
-//                .build();
-//    }
-//}
+package com.ezgibuseisik.todolistmyfinals.configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI getOpenAPIMethod() {
+        return new OpenAPI().info(new Info()
+                .title("Analyst Software Developer Ezgi Buse Işık")
+                .version("V1.0.0")
+                .description("Spring Boot & React Js & Devops")
+                .termsOfService("Software INC")
+                .contact(new Contact()
+                        .name("Ezgi Buse Işık")
+                        .email("ezgibuse159@gmail.com")
+                        .url("https://github.com/ezgibuse-akkaya")
+                )
+                .license(new License()
+                        .name("licence INC xyz")
+                        .url("https://github.com/ezgibuse-akkaya")
+                )
+        );
+    }
+}
--- src/main/java/com/ezgibuseisik/todolistmyfinals/controller/ToDoController.java
+++ src/main/java/com/ezgibuseisik/todolistmyfinals/controller/ToDoController.java
@@ -2,6 +2,9 @@ package com.ezgibuseisik.todolistmyfinals.controller;
 
 import com.ezgibuseisik.todolistmyfinals.model.ToDo;
 import com.ezgibuseisik.todolistmyfinals.service.IToDoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,36 +18,70 @@ import java.util.List;
 public class ToDoController {
     private final IToDoService toDoService;
 
+    @Operation(summary = "Get all todos", description = "Retrieve a list of all todos")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful retrieval of todos"),
+            @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
     @GetMapping
     public ResponseEntity<List<ToDo>> getToDos() {
         return new ResponseEntity<>(toDoService.getToDos(), HttpStatus.OK);
     }
 
+    @Operation(summary = "Add a new todo", description = "Create a new todo item")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Todo successfully created"),
+            @ApiResponse(responseCode = "400", description = "Invalid input")
+    })
     @PostMapping
     public ToDo addToDo(@RequestBody ToDo toDo) {
         return toDoService.addToDo(toDo);
     }
 
+    @Operation(summary = "Update an existing todo", description = "Update the details of an existing todo item")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Todo successfully updated"),
+            @ApiResponse(responseCode = "404", description = "Todo not found")
+    })
     @PutMapping("/update/{id}")
     public ToDo updateToDo(@RequestBody ToDo toDo, @PathVariable Long id) {
         return toDoService.updateToDo(toDo, id);
     }
 
+    @Operation(summary = "Update completion status", description = "Update the completion status of a todo item")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Completion status successfully updated"),
+            @ApiResponse(responseCode = "404", description = "Todo not found")
+    })
     @PutMapping("/updateStatus/{id}")
     public ToDo updateCompletionStatus(@PathVariable Long id, @RequestParam boolean done) {
         return toDoService.updateCompletionStatus(id, done);
     }
 
+    @Operation(summary = "Delete a todo", description = "Delete a todo item by ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Todo successfully deleted"),
+            @ApiResponse(responseCode = "404", description = "Todo not found")
+    })
     @DeleteMapping("/delete/{id}")
     public void deleteToDo(@PathVariable Long id) {
         toDoService.deleteToDo(id);
     }
 
+    @Operation(summary = "Get a todo by ID", description = "Retrieve a todo item by its ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful retrieval of todo"),
+            @ApiResponse(responseCode = "404", description = "Todo not found")
+    })
     @GetMapping("/todo/{id}")
     public ToDo getToDoById(@PathVariable Long id) {
         return toDoService.getToDoById(id);
     }
 
+    @Operation(summary = "Delete all todos", description = "Delete all todo items")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "All todos successfully deleted")
+    })
     @DeleteMapping("/deleteAll")
     public void deleteAllToDos() {
         toDoService.deleteAllToDos();
